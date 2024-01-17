9771585bdbdec6e470939ca9f1733b229f10e645
Lakshan Cooray <Lakshancooray23@gmail.com>
2024-01-17 10:32:51 +0530

    completed making the sketch design for main and tumour detection pages

---
+++ Lakshan/Web/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:56ca0f18ce48fc2ea1e25ce5bec748f99136c1074bd0cb843b6c8605175804be
+size 421
---
+++ Lakshan/Web/static/javaScript/BrainTumorDitector.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e9184fe044b5d4b4bb555902613e39060b74cd6b1a79d2499fd85615a82515b1
+size 2515
